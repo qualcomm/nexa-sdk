887f285b33bf9190e7d1fb9136700e34bd3a4c30
Hongzhi Chen <hongzhic@qti.qualcomm.com>
2026-04-16 13:12:17 +0800

    fix(sdk): missing opencl and hexagon param

--- sdk/cmake/llama_cpp.cmake
+++ sdk/cmake/llama_cpp.cmake
@@ -21,23 +21,18 @@ set(BUILD_SHARED_LIBS ON)
 
 set(GENIEX_LLAMA_CPP ON)
 
-# Find OpenSSL first without CMAKE_SYSTEM_PATH to prefer local installations
-if (CMAKE_SYSTEM_NAME STREQUAL "Windows" AND CMAKE_SYSTEM_PROCESSOR STREQUAL "arm64")
-    set(OpenCL_INCLUDE_DIR "$ENV{OPENCL_SDK_ROOT}/include")
-    set(OpenCL_LIBRARY "$ENV{OPENCL_SDK_ROOT}/lib/OpenCL.lib")
-endif()
-# add opencl lib for linux arm64
-# if (CMAKE_SYSTEM_NAME STREQUAL "Linux" AND CMAKE_SYSTEM_PROCESSOR STREQUAL "aarch64")
-#     set(OpenCL_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/linux-arm64/include")
-#     set(OpenCL_LIBRARY "${CMAKE_CURRENT_LIST_DIR}/linux-arm64/lib/OpenCL.so")
-# endif()
-# add opencl lib for android aarch64
-# if (CMAKE_SYSTEM_NAME STREQUAL "Android" AND CMAKE_SYSTEM_PROCESSOR STREQUAL "aarch64")
-#     set(OpenCL_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/android-arm64/include")
-#     set(OpenCL_LIBRARY "${CMAKE_CURRENT_LIST_DIR}/android-arm64/lib/libOpenCL.so")
-# endif()
-set(OPENCL_SDK_ROOT "$ENV{OPENCL_SDK_ROOT}")
+# patch for llama.cpp
+set(CMAKE_PREFIX_PATH "$ENV{OPENCL_SDK_ROOT}")
 set(HEXAGON_SDK_ROOT "$ENV{HEXAGON_SDK_ROOT}")
+set(HEXAGON_TOOLS_ROOT "$ENV{HEXAGON_TOOLS_ROOT}")
+if (CMAKE_SYSTEM_NAME STREQUAL "Windows" AND CMAKE_SYSTEM_PROCESSOR STREQUAL "arm64")
+    set(PREBUILT_LIB_DIR "windows_aarch64")
+elseif (CMAKE_SYSTEM_NAME STREQUAL "Linux" AND CMAKE_SYSTEM_PROCESSOR STREQUAL "aarch64")
+    set(PREBUILT_LIB_DIR "linux_aarch64")
+elseif (CMAKE_SYSTEM_NAME STREQUAL "Android" AND CMAKE_SYSTEM_PROCESSOR STREQUAL "aarch64")
+    set(PREBUILT_LIB_DIR "android_aarch64")
+endif()
+
 
 set(GENIEX_LLAMA_CPP_DIR "${CMAKE_SOURCE_DIR}/../third-party/llama.cpp"
     CACHE PATH "Path to llama.cpp source directory")
