d7c44ba352967865f254bc2d7d63d11182d7011d
Hongzhi Chen <hongzhic@qti.qualcomm.com>
2026-05-08 21:41:23 +0800

    style(sdk): tidy version.cpp include order and namespace comment

--- sdk/src/version.cpp
+++ sdk/src/version.cpp
@@ -1,7 +1,8 @@
-#include "build_config.h"
 #include "version.h"
+
+#include "build_config.h"
 
 namespace geniex::build_config {
 const char kBridgeVersion[]       = GENIEX_VERSION;
 const char kQairtRuntimeVersion[] = GENIEX_QAIRT_RUNTIME_VERSION;
-}
+}  // namespace geniex::build_config
